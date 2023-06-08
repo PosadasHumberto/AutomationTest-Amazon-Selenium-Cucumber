7685011e6d42c96301374bff26e9be47e6819a45
Humberto Posadas <posadas.humberto@outlook.com>
2023-06-08 11:22:14 +0200

    fixing Steps

--- src/test/java/steps/AmazonSteps.java
+++ src/test/java/steps/AmazonSteps.java
@@ -6,27 +6,27 @@ import io.cucumber.java.en.Then;
 
 public class AmazonSteps {
 
-    @Given("the user navigates to www.amazon.com")
+    @Given("^the user navigates to www.amazon.com$")
     public void navigateToAmazon(){
 
     }
 
-    @And("Searches for <Product>")
-    public void searchAProduct(){
+    @And("^Searches for (.*)$")
+    public void searchAProduct(String product){
 
     }
 
-    @And("navigates to the second page")
+    @And("^navigates to the second page$")
     public void navigateToPage(){
 
     }
 
-    @And("selects the third item")
+    @And("^selects the third item$")
     public void searchAnItem(){
 
     }
 
-    @Then("the user is able to add it to the cart")
+    @Then("^the user is able to add it to the cart$")
     public void validateAddingProductToTheCart(){
 
     }
